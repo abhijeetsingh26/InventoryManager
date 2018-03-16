8383b0bc219cfe6482fc37c0a68cac63c1328ebb
Abhijeet Rathore <abhijeet.rathore@faberwork.com>
2018-03-16 15:01:40 +0530

    Added barcode Module

--- app/src/main/java/com/sample/abhijeet/inventorymanager/MainActivity.java
+++ app/src/main/java/com/sample/abhijeet/inventorymanager/MainActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.sample.abhijeet.inventorymanager.Activity.BarcodeCaptureActivity;
 import com.sample.abhijeet.inventorymanager.Activity.ItemDetailsActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_BARCODE_CAPTURE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         FAB.setOnClickListener(new View.OnClickListener() {
                                    @Override
                                    public void onClick(View view) {
+                                       Intent intent = new Intent(MainActivity.this, BarcodeCaptureActivity.class);
+                                       intent.putExtra(BarcodeCaptureActivity.AutoFocus, true);
+                                       intent.putExtra(BarcodeCaptureActivity.UseFlash, false);
+
+                                       startActivityForResult(intent, RC_BARCODE_CAPTURE);
                                    }
                                }
 
