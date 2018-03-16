c63a004735e3027eb581a19f6edc504cce880777
Abhijeet Rathore <abhijeet.rathore@faberwork.com>
2018-03-16 19:11:54 +0530

    Test commit on Firebase branch

--- app/src/main/java/com/sample/abhijeet/inventorymanager/MainActivity.java
+++ app/src/main/java/com/sample/abhijeet/inventorymanager/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Listener for the main FAB button
         FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.addNewItemFAB);
         FAB.setOnClickListener(new View.OnClickListener() {
