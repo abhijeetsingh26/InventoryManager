718553d8fe775f781c4ab32d3c352ddf14807ae6
Abhijeet Rathore <abhijeet.rathore@faberwork.com>
2018-03-16 17:32:38 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/barcode_capture.xml

--- app/src/main/java/com/sample/abhijeet/inventorymanager/MainActivity.java
+++ app/src/main/java/com/sample/abhijeet/inventorymanager/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.sample.abhijeet.inventorymanager.Activity.BarcodeCaptureActivity;
 import com.sample.abhijeet.inventorymanager.Activity.ItemDetailsActivity;
 
@@ -35,4 +39,30 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE)
+        {
+            if (resultCode == CommonStatusCodes.SUCCESS)
+            {
+                if (data != null)
+                {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Toast.makeText(this,"Success, value= "  + barcode.displayValue, Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(this,"Failed to read barcode", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else
+            {
+            }
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
+
