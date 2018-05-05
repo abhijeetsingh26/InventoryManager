42fbbf450d6d4d0690f309be008ae4e4e97207e2
abhijeetsingh26 <abhijeetsingh26>
2018-05-05 12:53:20 +0530

    Revert "Revert "Added LiveData loading""
    This reverts commit 7b47a3d

---
+++ app/src/main/java/com/sample/abhijeet/inventorymanager/models/PurchaseDetailsViewModel.java
@@ -0,0 +1,57 @@
+package com.sample.abhijeet.inventorymanager.models;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class PurchaseDetailsViewModel  extends ViewModel
+{
+    private String TAG = PurchaseDetailsViewModel.class.getSimpleName();
+
+    private MutableLiveData<List<String>> fruitList;
+
+    public LiveData<List<String>> getFruitList() {
+        if (fruitList == null) {
+            fruitList = new MutableLiveData<>();
+            loadFruits();
+        }
+        return fruitList;
+    }
+
+    private void loadFruits() {
+        // do async operation to fetch users
+        Handler myHandler = new Handler();
+        myHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                List<String> fruitsStringList = new ArrayList<>();
+                fruitsStringList.add("Mango");
+                fruitsStringList.add("Apple");
+                fruitsStringList.add("Orange");
+                fruitsStringList.add("Banana");
+                fruitsStringList.add("Grapes");
+                long seed = System.nanoTime();
+                Collections.shuffle(fruitsStringList, new Random(seed));
+                fruitList.setValue(fruitsStringList);
+            }
+        }, 5000);
+
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.d(TAG, "on cleared called");
+    }
+}
